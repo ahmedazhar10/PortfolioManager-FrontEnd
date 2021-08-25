913e282502cf0ec132a3ca86a4ae390f290d63c9
Sam Silverstein <samsilverstein@Sams-MacBook-Pro.local>
2021-08-25 17:00:36 -0400

    added investments component

--- portfolio-manager-ang/src/app/app-routing.module.ts
+++ portfolio-manager-ang/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CashAccountComponent } from "./cash-account/cash-account.component";
+import { InvestmentComponent } from "./investment/investment.component";
 
 
 const routes: Routes = [
-  {path:'', redirectTo: '/cash-account', pathMatch:'full'},
-  {path:'cash-account', component:CashAccountComponent}
+  {path:'cash-account', component:CashAccountComponent},
+  {path:'investment', component:InvestmentComponent, outlet:'investment'}
 
 ];
 
