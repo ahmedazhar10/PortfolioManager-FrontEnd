9627dc8e5cf13a3f92ec26f5215f87d6207ffec7
Sam Silverstein <samsilverstein@Sams-MacBook-Pro.local>
2021-08-24 21:20:49 -0400

    angular basic architecture, with bootstrap

---
+++ portfolio-manager-ang/src/app/etf/etf.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-etf',
+  templateUrl: './etf.component.html',
+  styleUrls: ['./etf.component.css']
+})
+export class EtfComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
